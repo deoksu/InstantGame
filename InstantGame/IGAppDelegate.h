c6861399f310454dd640e7731850fc9fa8c4ad54
deoksu <apple@apple-ui-MacBook-Pro.local>
2014-07-14 13:55:02 +0900

    Initial Commit

---
+++ InstantGame/IGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IGAppDelegate.h
+//  InstantGame
+//
+//  Created by deoksu on 2014. 7. 14..
+//  Copyright (c) 2014년 kr.hs.mirim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstantGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstantGame
+//
+//  Created by deoksu on 2014. 7. 14..
+//  Copyright (c) 2014년 kr.hs.mirim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IGAppDelegate class]));
+    }
+}
